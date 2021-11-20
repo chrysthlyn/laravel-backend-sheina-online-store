9beacf83a6b164864f7edc48457193b3dd2fe74f
chrysthlyn <chrysthlyn@gmail.com>
2021-11-20 21:09:53 +0700

    Add model ProductGallery

---
+++ app/Models/ProductGallery.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class \ProductGallery extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'products_id', 'photo', 'is_default'
+    ];
+
+    protected $hidden = [];
+
+    public function product()
+    {
+        return $this->hasMany(ProductGallery::class, 'products_id');
+    }
+}

df70baaa9a9dfce8b9e05875ae3eddd1ec188c4c
chrysthlyn <chrysthlyn@gmail.com>
2021-11-20 21:18:03 +0700

    Fix some error

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class \ProductGallery extends Model
+class ProductGallery extends Model
 {
     use SoftDeletes;
 

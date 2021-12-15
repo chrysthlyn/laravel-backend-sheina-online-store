88fdc8608a026095097009a244425261c10895e1
chrysthlyn <chrysthlyn@gmail.com>
2021-12-15 23:07:34 +0700

    ProductControlelr: Add gallery function for list product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Http\Requests\ProductRequest;
-
+use App\Models\ProductGallery;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -115,5 +115,16 @@ class ProductController extends Controller
         $item->delete();
 
         return redirect()->route('products.index');
+    }
+
+    public function gallery(Request $request, $id)
+    {
+        $product = Product::findorFail($id);
+        $items = ProductGallery::with('product')->where('products_id', $id)->get();
+
+        return view('pages.products.gallery')->with([
+            'product' => $product,
+            'items' => $items
+        ]);
     }
 }

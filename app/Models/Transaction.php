81d5f8684ee0f7e477e2bb296c418e0a50228418
chrysthlyn <chrysthlyn@gmail.com>
2022-01-16 22:54:36 +0700

    Transaction Mode: Change some code

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,7 +10,7 @@ class Transaction extends Model
     use SoftDeletes;
 
     protected $fillable = [
-        'uuid', 'name', 'email', 'number', 'address', 'transaction_total', 'transaction_status'
+        'uid', 'name', 'email', 'number', 'address', 'transaction_total', 'transaction_status'
     ];
 
     protected $hidden = [];

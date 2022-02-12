07f077d5048d72f223684392d708777e77b996b9
chrysthlyn <chrysthlyn@gmail.com>
2022-02-12 23:58:32 +0700

    Transactions: Change some code in edit transactions page

--- resources/views/pages/transactions/edit.blade.php
+++ resources/views/pages/transactions/edit.blade.php
@@ -11,7 +11,7 @@
             @method('PUT')
             @csrf
             <div class="form-group">
-                <label for="name" class="form-control-label">Nama Barang</label>
+                <label for="name" class="form-control-label">Nama Pemesan</label>
                 <input type="text" name="name" value="{{ old('name') ? old('name') : $item->name }}"
                     class="form-control @error('name') is-invalid @enderror" />
                 @error('name')
@@ -19,7 +19,7 @@
                 @enderror
             </div>
             <div class="form-group">
-                <label for="email" class="form-control-label">Tipe Barang</label>
+                <label for="email" class="form-control-label">Email</label>
                 <input type="email" name="email" value="{{ old('email') ? old('email') : $item->email }}"
                     class="form-control @error('email') is-invalid @enderror" />
                 @error('email')

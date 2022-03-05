b4575e113a5d684d08fff9893174eba39101b428
chrysthyln <chrysthlyn@gmail.com>
2022-03-05 20:27:48 +0700

    Route: Add route transactions for set status

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Route::resource('products', 'ProductController');
 
 Route::resource('product-galleries', 'ProductGalleryController');
 
+Route::get('transactions/{id}/set-status', 'TransactionController@setStatus')->name('transactions.status');
 Route::resource('transactions', 'TransactionController');

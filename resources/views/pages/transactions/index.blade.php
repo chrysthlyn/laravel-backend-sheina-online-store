b700ccc1dc96c24acc92b38be0a321fd2765129a
chrysthyln <chrysthlyn@gmail.com>
2022-03-05 20:26:10 +0700

    Transactions Index: Add view set status

--- resources/views/pages/transactions/index.blade.php
+++ resources/views/pages/transactions/index.blade.php
@@ -47,10 +47,10 @@
                                     </td>
                                     <td>
                                         @if ($item->transaction_status == 'PENDING')
-                                        {{-- <a href="{{ route('transaction.status', $item->id) }}?status=SUCCESS"
-                                        class="btn btn-success btn-sm"><i class="fa fa-check"></i></a>
-                                        <a href="{{ route('transaction.status', $item->id) }}?status=FAILED"
-                                            class="btn btn-warning btn-sm"><i class="fa fa-times"></i></a> --}}
+                                        <a href="{{ route('transactions.status', $item->id) }}?status=SUCCESS"
+                                            class="btn btn-success btn-sm"><i class="fa fa-check"></i></a>
+                                        <a href="{{ route('transactions.status', $item->id) }}?status=FAILED"
+                                            class="btn btn-warning btn-sm"><i class="fa fa-times"></i></a>
                                         @endif
                                         <a href="#mymodal" data-remote="{{ route('transactions.show', $item->id) }}"
                                             data-toggle="modal" data-target="#mymodal"

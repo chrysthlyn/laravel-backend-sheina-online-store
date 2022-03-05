cb97fef5beb893bc3ee54dfc080b33458f898a69
chrysthyln <chrysthlyn@gmail.com>
2022-03-05 20:22:37 +0700

    Transaction Controlelr: Add function set status for transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -111,4 +111,18 @@ class TransactionController extends Controller
     {
         //
     }
+
+    public function setStatus(Request $request, $id)
+    {
+        $request->validate([
+            'status' => 'required|in:PENDING,SUCCESS,FAILED'
+        ]);
+
+        $item = Transaction::findOrFail($id);
+        $item->transaction_status = $request->status;
+
+        $item->save();
+
+        return redirect()->route('transactions.index');
+    }
 }

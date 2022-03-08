4db2055b5837c8e75f3f48d94184e51c9417989c
chrysthyln <chrysthlyn@gmail.com>
2022-03-09 00:31:21 +0700

    Add file ResponseFormatter for API

---
+++ app/Http/Controllers/API/ResponseFormatter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+class ResponseFormatter
+{
+    protected static $response = [
+        'meta' => [
+            'code' => 200,
+            'status' => 'success',
+            'message' => null,
+        ],
+        'data' => null
+    ];
+
+    public static function success($data = null, $message = null)
+    {
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['data']);
+    }
+}

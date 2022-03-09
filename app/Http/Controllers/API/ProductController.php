51fafbe7113efa5bbde664e055b38e8b7b92b4da
chrysthyln <chrysthlyn@gmail.com>
2022-03-09 19:21:29 +0700

    Add ProductController API

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+}

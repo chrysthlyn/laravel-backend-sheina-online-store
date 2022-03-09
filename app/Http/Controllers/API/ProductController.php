ffd4a77da6237fd6eda7be4b3c79d866c83a35e4
chrysthyln <chrysthlyn@gmail.com>
2022-03-09 20:06:59 +0700

    ProductController API: Add function all for get data

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -8,5 +8,14 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $name = $request->input('name');
+        $slug = $request->input('slug');
+        $type = $request->input('type');
+        $price_from = $request->input('price_from');
+        $price_to = $request->input('price_to');
+    }
 }
